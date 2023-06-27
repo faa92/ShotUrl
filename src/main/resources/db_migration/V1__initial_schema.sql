75643ae0739addfa5bcada19fa341e717222ae74
AntonFedoseev <fedoseev446@gmail.com>
2023-06-27 19:17:48 +0300

    fix

--- src/main/resources/db_migration/V1__initial_schema.sql
+++ src/main/resources/db_migration/V1__initial_schema.sql
@@ -1,22 +1,5 @@
-
-
-INSERT INTO  users_links (long_url)
-VALUES ('QWERTY');
-
-
 CREATE TABLE users_links (
-id          BIGSERIAL PRIMARY KEY,
-long_url    TEXT NOT NULL UNIQUE,
-shot_url    TEXT NOT NULL UNIQUE DEFAULT substr(md5(random()::text || clock_timestamp()::text), 1, 5)
+    id          BIGSERIAL PRIMARY KEY,
+    long_url    TEXT NOT NULL UNIQUE,
+    shot_url    TEXT NOT NULL UNIQUE DEFAULT substr(md5(random()::text || clock_timestamp()::text), 1, 5)
 );
-
-SELECT  shot_url
-FROM    users_links
-WHERE   long_url = 'QWERTY';
-
-DELETE FROM users_links
-WHERE long_url = 'QWERTY';
-
-SELECT *
-FROM users_links;
-
